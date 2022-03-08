9f67e89482d9dea5b6f05767cb3c447805de88c3
Tianrun Zhao <2672759Z@student.gla.ac.uk>
2022-03-08 18:34:43 +0000

    update for task manager

--- freshmen_world/static/js/base.js
+++ freshmen_world/static/js/base.js
@@ -1,9 +1,17 @@
-$(document).ready( function() {
+window.onload = function () {
 
-	$('.bar_link').on({
-		mouseover: function() {
-			$(this).css("background-color", "red") },
-		mouseleave: function() {
-			$(this).css("background-color", "skyblue") },
-	});
-});
+
+    document.getElementById("1").onclick = function () {
+        alert("About us\nOur team hopes to develop a website that can be used by major universities. \nWe hope you will support us!");
+    };
+    document.getElementById("2").onclick = function () {
+        alert("Terms\nWe hope you will respect our property rights!");
+    };
+
+    document.getElementById("3").onclick = function () {
+        alert("Help Documnet\nhttps://www.gla.ac.uk/myglasgow/students/\nhttps://www.gla.ac.uk/myglasgow/students/supportservices/");
+    };
+    document.getElementById("4").onclick = function () {
+        alert("Privacy and Cookies\nDo you accept all privacy and cookies on this website?");
+    };
+}
