28b490d345be2388e589687795061092de9aeba8
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-03-16 17:17:18 +0000

    Login changes

--- freshmen_world/static/js/base.js
+++ freshmen_world/static/js/base.js
@@ -1,17 +1,3 @@
-window.onload = function () {
-
-
-    document.getElementById("1").onclick = function () {
-        alert("About us\nOur team hopes to develop a website that can be used by major universities. \nWe hope you will support us!");
-    };
-    document.getElementById("2").onclick = function () {
-        alert("Terms\nWe hope you will respect our property rights!");
-    };
-
-    document.getElementById("3").onclick = function () {
-        alert("Help Documnet\nhttps://www.gla.ac.uk/myglasgow/students/\nhttps://www.gla.ac.uk/myglasgow/students/supportservices/");
-    };
-    document.getElementById("4").onclick = function () {
-        alert("Privacy and Cookies\nDo you accept all privacy and cookies on this website?");
-    };
-}
+$(document).ready( function() {
+    alert("Hello!");
+});
